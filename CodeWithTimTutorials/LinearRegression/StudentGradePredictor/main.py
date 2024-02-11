efada661f322a8c4176b6937010402dd60e0a019
unknown <emiljohnllanes772@gmail.com>
2024-02-11 17:07:05 +0800

    put everythin to main method

--- CodeWithTimTutorials/LinearRegression/StudentGradePredictor/main.py
+++ CodeWithTimTutorials/LinearRegression/StudentGradePredictor/main.py
@@ -3,4 +3,9 @@ import numpy
 from sklearn import linear_model;
 from sklearn.utils import shuffle;
 
-data = pandas.read_csv("student-mat.csv", sep=";")
+
+def main():
+    data = pandas.read_csv("student-mat.csv", sep=";")
+
+if __name__ == "__main__":
+    main()
