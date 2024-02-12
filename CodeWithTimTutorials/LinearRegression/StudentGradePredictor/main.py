5a5f1b070579297bc413cd571bca4b2f29619742
unknown <emiljohnllanes772@gmail.com>
2024-02-12 22:41:18 +0800

    add comments

--- CodeWithTimTutorials/LinearRegression/StudentGradePredictor/main.py
+++ CodeWithTimTutorials/LinearRegression/StudentGradePredictor/main.py
@@ -17,10 +17,10 @@ def main():
     # slices the data into parts so we can separate training and testing data and answers
     # useful for separating training data to testing data 
     training_data_train, training_data_test, actual_answers_train, actual_answers_test = train_test_split(attributes, predictions, train_size=0.25)
-    
+
+    # create a linear regression model and train a best fit line for it using training data and actual answers for training    
     linear = linear_model.LinearRegression()
     linear.fit(training_data_train, actual_answers_train)
-    acc = linear.score(training_data_test, actual_answers_test)
 
     # load pickle studentModel model 
     pickle_in = open("studentModel.pickle", "rb")
@@ -36,6 +36,7 @@ def main():
     # predict using training data 
     predictions = linear.predict(training_data_test)
 
+    # print prediction, data, and actual answers
     for i in range(len(predictions)):
         print(predictions[i], training_data_test[i], actual_answers_test[i])
 
