737b1e5b8dd8ac8de72b8f8ee30ff6d5ab45f89a
unknown <emiljohnllanes772@gmail.com>
2024-03-03 22:00:10 +0800

    implement neural network for fasion_mnist

---
+++ CodeWithTimTutorials/UnSupervisedLearning/NeuralNetworks/main.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+keras = tf.keras
+
+data = keras.datasets.fashion_mnist
+
+# special in keras because it makes it easier for us to write all this
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+# these label names corresponds to labels in train_labels; index 0 is T-shirt/top, 1 is Trouser.....etc etc
+label_names: list[str] = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Scale data to smaller number to make it easier for the computer to handle
+train_images = train_images/255.5
+test_images = test_images/255.5
+
+# This is the architecture of the neural network 
+# The input layer is a flatten 28 by 28, 2 dimensional array
+# One hidden layer with 128 neurons with activation of RELU
+# The output layer with 10 neurons with activation of soft max
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape = (28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+    ])
+
+# compile the model with these parameters 
+# TODO: research these parameters
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+# epochs is how many times you can see the same image
+model.fit(train_images, train_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print(f"ACC:\t {test_acc}")
